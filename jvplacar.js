a941903a12cc6319b588d2871820090a6248b94f
thas0ares <thais.rossi.1335121@sga.pucminas.br>
2021-08-13 22:44:47 -0300

    adicionando imagem

--- jvplacar.js
+++ jvplacar.js
@@ -5,20 +5,24 @@ const nomeA_HTML = document.getElementById('nomeTimeA') // função que retorna 
 const nomeB_HTML = document.getElementById('nomeTimeB')
 const pontoA_HTML = document.getElementById('pontuacaoA')
 const pontoB_HTML = document.getElementById('pontuacaoB')
+const setA_HTML = document.getElementById('setA')
+const setB_HTML = document.getElementById('setB')
 
 nomeA_HTML.innerText = nomeTimeA //Me dá o texto dentro do html
 nomeB_HTML.innerText = nomeTimeB
 
 let pontoA = 0
 let pontoB = 0
-pontoA_HTML.innerText = pontoA
+let setA = 0
+let setB = 0
+pontoA_HTML.innerText = pontoA // o que é exibido no placar do time A
 pontoB_HTML.innerText = pontoB
 
 pontoA_HTML.addEventListener('click', function () {
   //O que estiver aqui dentro será executado, quando o pontoA_HTML for clicado
   // pontoB_HTML.addEventListener*qual html*('click*qual evento você quer escutar*', function ()*função anonima* {
   //quando você dectecta que um usuário clica na tela
-  pontoA = pontoA + 1 // outra opção pe pontoA++
+  pontoA = pontoA + 1 // outra opção pe pontoA++ => soma 1 ao valor anterior
   pontoA_HTML.innerText = pontoA
   verificarFimDoSet()
 })
@@ -30,14 +34,29 @@ pontoB_HTML.addEventListener('click', function () {
 })
 
 function verificarFimDoSet() {
-  if (pontoA >= 21) {
+  if (pontoA >= 21 && pontoA - pontoB >= 2) {
+    // nessa função criamos a diferença de dois ponto que o time tem que ter para se ganhar um set **&&** E ** sendo verdadeiro
     // if -> condição, o que faze se a condição for verdadeira
     alert('Time A venceu o set!')
+    setA = setA + 1 // soma 1 ao valor anterior do set
+    setA_HTML.innerText = setA // o que de fato exibe na tela
+    zerarPlacardoSetAnterior() // para rodar e ir para a outra função.
   } //else {  else-> o que fazer se a condição for falsa
   // alert('time A não venceu ainda...')}
-  if (pontoB >= 21) {
+  if (pontoB >= 21 && pontoB - pontoA >= 2) {
     alert('Time B venceu o set!')
+    setB = setB + 1
+    setB_HTML.innerText = setB
+    zerarPlacardoSetAnterior()
   }
+}
+function zerarPlacardoSetAnterior() {
+  //Zerar o placar, pra iniciar um novo set
+  pontoA = 0
+  pontoB = 0
+  //atulizar a tela
+  pontoA_HTML.innerText = pontoA
+  pontoB_HTML.innerText = pontoB
 }
 
 //----------------------------------------------------------
